fffe5f59b8f197770c51df7f2e12234605f28399
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 18:31:06 +0100

    rust: Rename callback receivers.

--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -12,10 +12,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
                                           // Don't use device before ipcon is connected.
 
-    // Create receiver for decibel events.
-    let decibel_receiver = spl.get_decibel_receiver();
+    let decibel_receiver = spl.get_decibel_callback_receiver();
 
-    // Spawn thread to handle received events. This thread ends when the `spl` object
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `spl` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
         for decibel in decibel_receiver {
