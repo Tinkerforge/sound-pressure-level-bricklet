90f5265261d07bddcd73cdf0e5d7c89cd2df258b
Erik Fleckstein <erik@tinkerforge.com>
2018-12-14 16:16:07 +0100

    Add go examples.

---
+++ software/examples/go/example_callback.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/ipconnection"
+	"tinkerforge/sound_pressure_level_bricklet"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Sound Pressure Level Bricklet.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	spl, _ := sound_pressure_level_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	spl.RegisterDecibelCallback(func(decibel uint16) {
+		fmt.Printf("Decibel: %d dB(A)\n", float64(decibel)/10.0)
+	})
+
+	// Set period for decibel callback to 1s (1000ms) without a threshold.
+	spl.SetDecibelCallbackConfiguration(1000, false, 'x', 0, 0)
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
---
+++ software/examples/go/example_simple.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/ipconnection"
+	"tinkerforge/sound_pressure_level_bricklet"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Sound Pressure Level Bricklet.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	spl, _ := sound_pressure_level_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	// Get current decibel.
+	decibel, _ := spl.GetDecibel()
+	fmt.Printf("Decibel: %f dB(A)\n", float64(decibel)/10.0)
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
