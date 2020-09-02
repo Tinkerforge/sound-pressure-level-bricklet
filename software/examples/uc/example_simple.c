16732cccddfa9e2b579bf8156004c10c22ff898a
Erik Fleckstein <erik@tinkerforge.com>
2020-09-02 09:57:34 +0200

    uc: Add example driver note.

--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -1,3 +1,7 @@
+// This example is not self-contained.
+// It requres usage of the example driver specific to your platform.
+// See the HAL documentation.
+
 #include "bindings/hal_common.h"
 #include "bindings/bricklet_sound_pressure_level.h"
 
